5fec891d9deb92b53e34623cb6dfe19aa5f7fa82
Justin Torrance <justin.torrance91@gmail.com>
2019-01-09 00:46:19 -0700

    Write test for fetchPetsByShelter

---
+++ src/thunks/fetchPetsByShelter.test.js
@@ -0,0 +1,22 @@
+import { fetchPetsByShelter } from './fetchPetsByShelter'
+import { fetchPets }  from '../actions/index'
+
+const API_KEY = `${process.env.REACT_APP_API_KEY}`
+const shelterID = 'C0469'
+
+describe('fetchPetsByShelter', () => {
+  it('should dispatch fetchPets', async () => {
+    const mockPets = [{name: 'fluffy'}, {name: 'bigsby'}]
+    const mockDispatch = jest.fn()
+    window.fetch = jest.fn().mockImplementation(() => Promise.resolve({
+      ok: true,
+      json: () => Promise.resolve({
+        pets: mockPets
+      })
+    }))
+    const mockUrl = 'https://cors-anywhere.herokuapp.com/http://api.petfinder.com/shelter.getPets?key=' + API_KEY + '&id=' + shelterID + '&format=json&output=full'
+    const thunk = fetchPetsByShelter(mockUrl)
+    await thunk(mockDispatch)
+    expect(mockDispatch).toHaveBeenCalledWith(fetchPetsByShelter)
+  })
+})
